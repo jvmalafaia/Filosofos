76bfdaea7235047958d22c8005f048eedf0dbbe4
jvmalafaia <47794048+jvmalafaia@users.noreply.github.com>
2021-03-23 22:15:59 -0300

    Update 1.ComDeadlock.ts

--- 1.ComDeadlock.ts
+++ 1.ComDeadlock.ts
@@ -1,52 +1,129 @@
-import { Thread, ThreadStart } from 'thread';
-import { Dinner } from './';
+using System;
+using System.Threading;
 
-export class Philo {
-  private _thread: Thread;
-  public _status: number = 0;
-  private _spoons: number = 1;
+namespace filosofos
+{
+    public class Filosofo
+    {
+        public Filosofo(int id)
+        {
+            this.Id = id;
+            this.thread = new Thread(new ThreadStart(this.Eat));
+        }
+        public int Id { get; set; }
+        public Thread thread { get; set; }
+        public char Status { get; set; } = 'P';
+        public bool IsEating => Status == 'E';
+        public void Start()
+        {
+            thread.Start();
+        }
+        public void Stop()
+        {
+            DevolverHashi();
+            thread.Interrupt();
+        }
+        public void PegarHasi()
+        {
+            HashiCount += 1;
+            Program.totalHashis -= 1;
+        }
+        public void DevolverHashi()
+        {
+            HashiCount -= 1;
+            Program.totalHashis += 1;
+        }
 
-  constructor(private _id: number, private _dinner: Dinner) {
-    this._thread = new Thread(new ThreadStart(this.eat));
-  }
+        public int HashiCount { get; set; } = 1;
 
-  public start = () => {
-    try {
-      this._thread.start();
-    } catch (error) {
-      console.log(`\n ${this._id} dead`);
-      throw new Error(error);
+        public void Wait(Filosofo b)
+        {
+            Console.Write($" {this} Let's wait.... ");
+            if (b == null) return;
+            lock (b)
+            {
+                while (b.IsEating) ;
+                return;
+            }
+        }
+
+        private void Eat()
+        {
+            if (HashiCount < 2 && Program.totalHashis < 2) return;
+            Status = 'E';
+            Program.filosofosComendo += 1;
+            Program.filosofosPensando -= 1;
+            Thread.Sleep(10 / HashiCount);
+            Status = 'P';
+            Program.filosofosPensando += 1;
+            Program.filosofosComendo -= 1;
+            Stop();
+            thread = new Thread(new ThreadStart(Eat));
+        }
+
+        public override string ToString()
+        {
+            return $"[{typeof(Filosofo)} {Id}] {Status}";
+        }
     }
-  };
+}
 
-  private stop = () => {
-    this.dropSpoon();
-    this._thread.Interrupt();
-  };
+using System;
+using System.Collections.Generic;
 
-  private eat = () => {
-    if (this._spoons < 2 && this._dinner._totalSpoons < 2) return;
-    this._status = 1;
-    this._dinner._totalEatingPhilos += 1;
-    this._dinner._totalThinkingPhilos -= 1;
-
-    Thread.Sleep((100 * Math.random()) / this._spoons / 10);
-
-    this._status = 0;
-    this._dinner._totalThinkingPhilos += 1;
-    this._dinner._totalEatingPhilos -= 1;
-
-    this.stop();
-    this._thread = new Thread(new ThreadStart(this.eat));
-  };
-
-  public getSpoon = () => {
-    this._spoons += 1;
-    this._dinner._totalSpoons -= 1;
-  };
-
-  private dropSpoon = () => {
-    this._spoons -= 1;
-    this._dinner._totalSpoons += 1;
-  };
+namespace filosofos
+{
+    class Program
+    {
+        public const int totalFilosofos = 5;
+        public static int totalHashis = 5;
+        public static int filosofosPensando = 0;
+        public static int filosofosComendo = 0;
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<Filosofo> filosofos = new List<Filosofo>();
+            for (int i = 0; i < Program.totalFilosofos; i++)
+            {
+                filosofos.Add(new Filosofo(i));
+            }
+            int ciclos = 1;
+            List<decimal> percentile = new List<decimal>();
+            while (true)
+            {
+                try
+                {
+                    Filosofo lastToEat = null;
+                    foreach (var filosofo in filosofos)
+                    {
+                        if (Program.totalHashis > 1 && Program.filosofosComendo < 2)
+                        {
+                            filosofo.PegarHasi();
+                            lastToEat = filosofo;
+                            filosofo.Start();
+                            continue;
+                        }
+                        if (filosofo.Status == 'P')
+                        {
+                            filosofo.Wait(lastToEat);
+                            filosofosPensando += 1;
+                        }
+                        Console.Write($"{filosofo} ");
+                    }
+                    Console.WriteLine();
+                    percentile.Add( Program.filosofosPensando / Program.filosofosComendo );
+                    if (Program.filosofosPensando == totalFilosofos) break;
+                    ciclos += 1;
+                    filosofosPensando = 0;
+                }
+                catch{
+                    break;
+                }
+            }
+            for (int i = 0; i < percentile.Count; i++)
+            {
+                Console.WriteLine($"ciclo: {i + 1} || percentile tpensando/tcomendo: {percentile[i]} ");
+            }
+        }
+    }
 }
